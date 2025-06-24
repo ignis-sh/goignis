4c765481fe6ae526eed8fdb5865a5d4359e945fc
lost-melody <lost__melody@163.com>
2025-06-24 21:20:29 +0800

    init cobra structure

---
+++ cmd/root.go
@@ -0,0 +1,24 @@
+package cmd
+
+import (
+	"github.com/spf13/cobra"
+)
+
+// rootCmd represents the base command when called without any subcommands
+var rootCmd = &cobra.Command{
+	Use:   "goignis",
+	Short: "An optional CLI for ignis",
+}
+
+// Execute adds all child commands to the root command and sets flags appropriately.
+// This is called by main.main(). It only needs to happen once to the rootCmd.
+func Execute() {
+	err := rootCmd.Execute()
+	if err != nil {
+		os.Exit(1)
+	}
+}
+
+func init() {
+	rootCmd.PersistentFlags().BoolP("json", "j", false, "Print results in json")
+}
---
+++ main.go
@@ -0,0 +1,9 @@
+package main
+
+import (
+	"github.com/lost-melody/goignis/cmd"
+)
+
+func main() {
+	cmd.Execute()
+}
